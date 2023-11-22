254733879a9971ee5a855d8211f595c48a0567ef
yongikim <yongi.kim08@gmail.com>
2023-11-22 09:48:23 +0900

    Add s3.tf
    Finish chapter 6

---
+++ s3.tf
@@ -0,0 +1,96 @@
+resource "aws_s3_bucket" "private" {
+  bucket = "private-pragmatic-terraform-on-aws-yongi-kim"
+}
+
+resource "aws_s3_bucket_public_access_block" "private" {
+  bucket = aws_s3_bucket.private.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
+resource "aws_s3_bucket_versioning" "versioning" {
+  bucket = aws_s3_bucket.private.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "private" {
+  bucket = aws_s3_bucket.private.id
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
+}
+
+resource "aws_s3_bucket" "public" {
+  bucket = "public-pragmatic-terraform-on-aws-yongi-kim"
+}
+
+resource "aws_s3_bucket_public_access_block" "public" {
+  bucket = aws_s3_bucket.public.id
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+
+resource "aws_s3_bucket_cors_configuration" "public" {
+  bucket = aws_s3_bucket.public.id
+
+  cors_rule {
+    allowed_origins = ["https://example.com"]
+    allowed_methods = ["GET"]
+    allowed_headers = ["*"]
+    max_age_seconds = 3000
+  }
+}
+
+resource "aws_s3_bucket_ownership_controls" "public" {
+  bucket = aws_s3_bucket.public.id
+
+  rule {
+    object_ownership = "BucketOwnerEnforced"
+  }
+}
+
+resource "aws_s3_bucket" "alb_log" {
+  bucket = "alb-log-pragmatic-terraform-on-aws-yongi-kim"
+}
+
+resource "aws_s3_bucket_lifecycle_configuration" "alb_log" {
+  bucket = aws_s3_bucket.alb_log.id
+
+  rule {
+    id     = "alb_log"
+    status = "Enabled"
+
+    expiration {
+      days = 180
+    }
+  }
+}
+
+resource "aws_s3_bucket_policy" "alb_log" {
+  bucket = aws_s3_bucket.alb_log.id
+  policy = data.aws_iam_policy_document.alb_log.json
+}
+
+data "aws_iam_policy_document" "alb_log" {
+  statement {
+    effect    = "Allow"
+    actions   = ["s3:PutObject"]
+    resources = ["arn:aws:s3:::${aws_s3_bucket.alb_log.id}/*}"]
+
+    principals {
+      type        = "AWS"
+      identifiers = ["651404961923"]
+    }
+  }
+}
